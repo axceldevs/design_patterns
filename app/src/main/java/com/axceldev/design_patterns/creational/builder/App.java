e7e8e2edc3f65d65e30fcda01d17bb76a5fb67cb
cristian bambague <axceldev98@gmail.com>
2026-03-11 22:31:53 -0500

    Init project

---
+++ app/src/main/java/com/axceldev/design_patterns/creational/builder/App.java
@@ -0,0 +1,8 @@
+package com.axceldev.design_patterns.creational.builder;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.print("Hello init builder pattern");
+    }
+    
+}
